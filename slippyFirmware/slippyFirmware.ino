1c9ef1af9e57b564c9d171088e9cd72633b9e662
WattleFoxxo <88825700+WattleFoxxo@users.noreply.github.com>
2023-07-23 20:53:40 +1000

    added api mode

--- slippyFirmware/slippyFirmware.ino
+++ slippyFirmware/slippyFirmware.ino
@@ -3,14 +3,39 @@
 
 #define NODE_ADDRESS 0
 
+/* CONFIG */
+
+//LoRa
 #define LORA_CS 10
 #define LORA_RST 9
 #define LORA_INT 2
-
 #define LORA_FREQ 915.0
+
+//Serial
+#define HEADLESS true
+#define HUMAN_READABLE true
+
+/* ------ */
+
+
+/* 
+  Serial codes
+    [IN] <address> <message> : Incoming message
+    [OUT] <address> <message> : Outgoing message
+    [STATUS] <code> (<value>)... : Status message
+
+  Serial commands
+    *[SEND] <address> <massage> : Send a message
+    *[SET] <veriable> <value>... : Set a value
+
+  (all marked with "*" is unimplemented)
+*/
 
 RH_RF95 rf95(LORA_CS, LORA_INT);
 RHMesh rfMesh(rf95, NODE_ADDRESS);
+
+char serialBuffer[RH_MESH_MAX_MESSAGE_LEN];
+int serialBufferIndex = 0;
 
 void setup() {
   randomSeed(analogRead(0));
@@ -18,7 +43,11 @@ void setup() {
   Serial.begin(9600);
   while (!Serial); // Remove for headless
 
-  Serial.println("[INFO] SlippyMesh Starting...");
+  if (HUMAN_READABLE) {
+    Serial.println("SlippyMesh Starting...");
+  } else {
+    Serial.print("[STATUS] STARTING");
+  }
 
   // reset the LoRa module or it will have alex.d behavior
   pinMode(LORA_RST, OUTPUT);
@@ -30,33 +59,94 @@ void setup() {
   delay(50);
 
   if (!rfMesh.init()) {
-    Serial.println("[ERROR] Failed to init LoRa.");
+    if (HUMAN_READABLE) {
+      Serial.println("Failed to init LoRa.");
+    } else {
+      Serial.println("[STATUS] ERROR_INIT");
+    }
     while(1);
   }
 
+  rf95.setModemConfig(RH_RF95::ModemConfigChoice::Bw125Cr45Sf2048);
+
   if (!rf95.setFrequency(LORA_FREQ)) {
-    Serial.println("[ERROR] Failed to set LoRa frequency.");
+    if (HUMAN_READABLE) {
+      Serial.println("Failed to set LoRa frequency.");
+    } else {
+      Serial.println("[STATUS] ERROR_LORA_FREQ");
+    }
     while(1);
   }
 
   rfMesh.setTimeout(3000);
   rfMesh.setRetries(5);
 
-  Serial.println("[INFO] Looking for available address.");
+
+  if (HUMAN_READABLE) {
+    Serial.println("Looking for available address.");
+  } else {
+    Serial.print("[STATUS] LOOKING");
+  }
 
   uint8_t address = findAvailableAddress();
   rfMesh.setThisAddress(address);
 
-  Serial.println("[INFO] Mesh network initialized.");
+  if (HUMAN_READABLE) {
+    Serial.println("Mesh network initialized.");
+  } else {
+    Serial.print("[STATUS] READY");
+  }
 }
 
+void(* resetFunc) (void) = 0; //declare reset function @ address 0
+
 void loop() {
-  if (Serial.available()) {
-    String incomingString = Serial.readStringUntil('\n');
-    Serial.print("[INFO] Sending: ");
-    Serial.print(incomingString);
-    Serial.println(" to: 255");
-    sendMeshMessage(incomingString.c_str(), 255); // everyone address
+  while (Serial.available() > 0) {
+    char data = Serial.read();
+    if (data == '\n') {
+      serialBuffer[serialBufferIndex] = '\0';
+      //remove(serialBuffer, 7); // removes "[SEND] " // TODO: get acual command
+
+      int addr = atoi(serialBuffer); // gets address
+      remove(serialBuffer, indexOf(serialBuffer, ';')+1); // removes the address
+
+      if (addr > 0 && addr < 256) {
+        if (HUMAN_READABLE) {
+          Serial.print("Sending \"");
+          Serial.print(serialBuffer);
+          Serial.print("\" to node #");
+          Serial.println(addr);
+        } else {
+          Serial.print("[OUT] ");
+          Serial.print(addr);
+          Serial.print(" ");
+          printASCII(serialBuffer);
+          Serial.println("");
+        }
+
+        sendMeshMessage(serialBuffer, addr);
+      } else {
+        if (HUMAN_READABLE) {
+          Serial.println("Invalid address.");
+        }
+      }
+
+      serialBufferIndex = 0;
+      memset(serialBuffer, 0, sizeof(serialBuffer));
+    } else {
+      if (serialBufferIndex < RH_MESH_MAX_MESSAGE_LEN - 1) {
+        serialBuffer[serialBufferIndex] = data;
+        serialBufferIndex++;
+      }
+    }
+  }
+
+  if (millis() % 5000 == 0) {
+    sendMeshMessage("5g vaccine chip acivate", 1);
+  }
+
+  if (millis() >= 300000) {
+    resetFunc();
   }
 
   if (rfMesh.available()) {
@@ -67,10 +157,19 @@ void loop() {
 
     if (rfMesh.recvfromAck(buf, &len, &from, &dest)) {
       if (dest == rfMesh.thisAddress() || dest == 255 ) {
-        Serial.print("[DATA];");
-        Serial.print(from);
-        Serial.print(";");
-        Serial.println((char*)buf);
+        if (HUMAN_READABLE) {
+          Serial.print("Message from node #");
+          Serial.print(from);
+          Serial.print(", \"");
+          Serial.print((char*)buf);
+          Serial.println("\"");
+          sendMeshMessage(buf, from);
+        } else {
+          Serial.print("[IN] ");
+          Serial.print(from);
+          Serial.print(" ");
+          Serial.println((char*)buf);
+        }
       }
     }
   }
@@ -78,24 +177,70 @@ void loop() {
 
 void sendMeshMessage(char* message, uint8_t dest) {
   if (rfMesh.sendtoWait((uint8_t*)message, strlen(message) + 1, dest) != RH_ROUTER_ERROR_NONE) {
-    Serial.println("[ERROR] Message failed to send.");
+    if (HUMAN_READABLE) {
+      Serial.println("Message failed to send.");
+    } else {
+      Serial.println("[STATUS] ERROR_SEND");
+    }
   }
 }
 
 uint8_t findAvailableAddress() {
-  uint8_t nodeId = 1;
+  unt8_t nodeId = 1;
 
   while (nodeId <= 255) {
     if (rfMesh.sendtoWait((uint8_t*)"PING", 5, nodeId) == RH_ROUTER_ERROR_NO_ROUTE) {
-      Serial.print("[INFO] Found available address, Assigned: ");
-      Serial.print(nodeId);
-      Serial.println(".");
+      if (HUMAN_READABLE) {
+        Serial.print("Found available address, Assigned #");
+        Serial.print(nodeId);
+        Serial.println(".");
+      } else {
+        Serial.print("[STATUS] ID ");
+        Serial.println(nodeId);
+      }
       return nodeId;
     }
 
     nodeId++;
   }
 
-  Serial.println("[ERROR] No available address found.");
+  if (HUMAN_READABLE) {
+    Serial.println("No available address found.");
+  } else {
+    Serial.println("[STATUS] ERROR_NO_ADDR");
+  }
   while (1);
+  //return 192;
+}
+
+// helpers
+
+int indexOf(char *str, char searchChar) {
+  int len = strlen(str);
+  for (int i = 0; i < len; i++) {
+    if (str[i] == searchChar) {
+      return i;
+    }
+  }
+  return 0;
+}
+
+void remove(char* str, int n) {
+    int len = strlen(str);
+    if (n >= len) {
+        str[0] = '\0';
+    } else {
+        memmove(str, str + n, len - n + 1);
+    }
+}
+
+void printASCII(char* data) {
+  Serial.print("[");
+  for (int i = 0; i < strlen(data); i++) {
+    if (i > 0) {
+      Serial.print(", ");
+    }
+    Serial.print((int)data[i]);
+  }
+  Serial.print("]");
 }
