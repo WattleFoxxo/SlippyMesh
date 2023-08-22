203b3176f70bf1e663ad44067cc23c499f838ef7
WattleFoxxo <88825700+WattleFoxxo@users.noreply.github.com>
2023-08-22 20:24:55 +1000

    removed junk and edited modem settings

--- slippyFirmware/slippyFirmware.ino
+++ slippyFirmware/slippyFirmware.ino
@@ -2,20 +2,18 @@
 #include <LoRa.h> /* https://github.com/sandeepmistry/arduino-LoRa */
 #include <ArduinoUniqueID.h> /* https://github.com/ricaun/ArduinoUniqueID */
 
+/*
+ip of mnt talor 57.15.21.28
+*/
+
 #define LORA_CS 10
 #define LORA_RST 9
 #define LORA_IRQ 2
-
 #define LORA_FRQ 915E6
 
-#define MAX_BUFFER 64
-#define MSG_ID_BUCKET_SIZE 8
-
-#define NET_VERSION 1
-#define MESSAGE_INFO true
-#define LOG_INFO true
-#define LOG_WARNING true
-#define LOG_ERROR true
+#define MAX_BUFFER 128
+#define MSG_ID_BUCKET_SIZE 16
+#define NET_VERSION 2
 
 const uint8_t BROADCAST_ADDRESS[4] = {255, 255, 255, 255};
 
@@ -34,87 +32,74 @@ uint8_t msgIdBucketPos = 0;
 
 void setup() {
   Serial.begin(115200);
-  info("Starting slippy..\n");
+  Serial.println("INIT"); // "Starting slippy.."
 
   randomSeed(analogRead(0));
 
   LoRa.setPins(LORA_CS, LORA_RST, LORA_IRQ);
 
   if (!LoRa.begin(LORA_FRQ)) {
-    error("LoRa module failure.\n");
-    while (true);
+    Serial.println("LORA_FAIL"); // "LoRa module failure!"
+    while(true);
   }
 
-  LoRa.setSpreadingFactor(12);
-  LoRa.setSignalBandwidth(125E3);
-  LoRa.setCodingRate4(5);
-  LoRa.setTxPower(20);
-  LoRa.enableCrc();
+  // LoRa.setSpreadingFactor(12);
+  // LoRa.setSignalBandwidth(125E3);
+  // LoRa.setCodingRate4(5);
+  // LoRa.setTxPower(20);
+  // LoRa.enableCrc();
+
+  LoRa.setTxPower(20);                   // Supported values are 2 to 20
+  LeRa.setSpreadingFactor(12);           // Supported values are between 6 and 12
+  LoRa.setSignalBandwidth(62.5E3);       // Supported values are 7.8E3, 10.4E3, 15.6E3, 20.8E3, 31.25E3, 41.7E3, 62.5E3, 125E3, 250E3, and 500E3.
+  LoRa.setCodingRate4(8);                // Supported values are between 5 and 8, these correspond to coding rates of 4/5 and 4/8
+  LoRa.setGain(6);                       // Supported values are between 0 and 6
 
   getAddress(localAddress);
   
-  info("Your address is: ");
-  printAddress(localAddress, ".");
+  Serial.print("READY "); // "Rady, Your address is: <address>""
+  printArray(localAddress, ".", 4);
   Serial.print("\n");
 }
 
 void loop() {
-  while (Serial.available()) {
-    static char serialBuffer[MAX_BUFFER];
-    static unsigned int serialBufferPos = 0;
+  if (Serial.available() > 0) {
+    char serialBuffer[512];
+    char *token;
 
-    char inByte = Serial.read();
+    byte numBytes = Serial.readBytesUntil('\n', serialBuffer, 512);
+    token = strtok(serialBuffer, " ");
 
-    if (inByte != '\n') {
-      serialBuffer[serialBufferPos] = inByte;
-      serialBufferPos += 1;
-    } else {
-      serialBuffer[serialBufferPos] = '\0';
-      serialBufferPos = 0;
-
-      char* cmdSplit[2];
-      splitCharArray(serialBuffer, ';', cmdSplit, 2);
-
-      char* message = cmdSplit[1];
-
-      char* ipSplit[4];
-      splitCharArray(cmdSplit[0], '.', ipSplit, 4);
-      
-      Packet newPacket;
-
-      for (int i = 0; i < 4; i++) {
-        newPacket.dest[i] = atoi(ipSplit[i]);
-      }
-
-      for (int i = 0; i < 4; i++) {
-        newPacket.src[i] = localAddress[i];
-      }
-
-      for (int i = 0; i < 4; i++) {
-        newPacket.msg_id[i] = random(256);
-      }
-
-      newPacket.buffer_len = strlen(message);
-
-      char tempBuff[newPacket.buffer_len];
-      for (int i = 0; i < newPacket.buffer_len; i++) {
-        tempBuff[i] = message[i];
-      }
-
-      newPacket.buffer = tempBuff;
-
-      addIDtoMsgBucket(newPacket.msg_id);
-
-      sendPacket(newPacket);
-
-      info("Sent message.\n");
+    Packet newPacket;
+    
+    for (int i = 0; i < 4; i++) {
+      newPacket.dest[i] = atoi(strtok(NULL, " "));
     }
+
+    for (int i = 0; i < 4; i++) {
+      newPacket.src[i] = localAddress[i];
+    }
+
+    for (int i = 0; i < 4; i++) {
+      newPacket.msg_id[i] = random(256);
+    }
+    
+    newPacket.buffer_len = atoi(strtok(NULL, " "));
+
+    char tempBuff[newPacket.buffer_len];
+    for (int i = 0; i < newPacket.buffer_len; i++) {
+      tempBuff[i] = (char)atoi(strtok(NULL, " "));
+    }
+    newPacket.buffer = tempBuff;
+
+    sendPacket(newPacket);
   }
 
   readPacket(LoRa.parsePacket());
 }
 
 void sendPacket(Packet packet) {
+  Serial.println("SENDING"); // "Sending..."
   LoRa.beginPacket();
 
   LoRa.write(NET_VERSION);
@@ -138,6 +123,7 @@ void sendPacket(Packet packet) {
   }
 
   LoRa.endPacket();
+  Serial.println("SENT"); // "Sent."
 }
 
 void readPacket(int packetSize) {
@@ -173,7 +159,7 @@ void readPacket(int packetSize) {
   newPacket.buffer = tempBuff;
 
   if (checkBucket(newPacket.msg_id)) {
-    info("Alrady recived this message, Ignoring.\n");  
+    Serial.println("OLD_MSG"); // "Alrady recived this message, Ignoring."  
     return;
   }
 
@@ -181,41 +167,82 @@ void readPacket(int packetSize) {
 
   if (compAddress(newPacket.dest, localAddress)) {
     printPacket(newPacket);
+
+    if (compBuffer(newPacket.buffer, "PING", 4)) {
+      Packet pongPacket;
+
+      for (int i = 0; i < 4; i++) {
+        pongPacket.dest[i] = newPacket.src[i];
+      }
+
+      for (int i = 0; i < 4; i++) {
+        pongPacket.src[i] = localAddress[i];
+      }
+
+      for (int i = 0; i < 4; i++) {
+        pongPacket.msg_id[i] = random(256);
+      }
+
+      pongPacket.buffer_len = 4;
+
+      pongPacket.buffer = "PONG";
+
+      sendPacket(pongPacket);
+    }
   } else if (compAddress(newPacket.dest, {BROADCAST_ADDRESS})){
-    printPacket(newPacket);
+    //printPacket(newPacket);
+    
+    if (compBuffer(newPacket.buffer, "PING", 4)) {
+      Packet pongPacket;
+
+      for (int i = 0; i < 4; i++) {
+        pongPacket.dest[i] = newPacket.src[i];
+      }
+
+      for (int i = 0; i < 4; i++) {
+        pongPacket.src[i] = localAddress[i];
+      }
+
+      for (int i = 0; i < 4; i++) {
+        pongPacket.msg_id[i] = random(256);
+      }
+
+      pongPacket.buffer_len = 4;
+
+      pongPacket.buffer = "PONG";
+
+      sendPacket(pongPacket);
+    }
+    
     sendPacket(newPacket);
   } else {
+    Serial.println("WRONG_ADDR"); // "This packet is not for me, Rebroadcasting"
     sendPacket(newPacket);
   }
 }
 
+void sendPong() {
+  
+      
+}
+
 void printPacket(Packet packet) {
-  Serial.println("-------[MESSAGE]-------");
-  Serial.print("Message: \"");
-  for (int i = 0; i < packet.buffer_len; i++) {
-    Serial.print((char)packet.buffer[i]);
-  }
-  Serial.print("\"\n");
-  Serial.print("To: ");
-  printAddress(packet.dest, ".");
-  Serial.print("\nFrom: ");
-  printAddress(packet.src, ".");
+  Serial.print("MESSAGE ");
+
+  printArray(packet.dest, ".", 4);
+  Serial.print(" ");
+  printArray(packet.src, ".", 4);
+  Serial.print(" ");
+  printArray(packet.msg_id, "", 4);
+  Serial.print(" ");
+  Serial.print(packet.buffer_len, DEC);
+  Serial.print(" ");
+  printArray(packet.buffer, ",", packet.buffer_len);
+  Serial.print(" ");
+  Serial.print(LoRa.packetRssi(), DEC);
+  Serial.print(" ");
+  Serial.print(LoRa.packetSnr(), DEC);
   Serial.print("\n");
-
-  if (MESSAGE_INFO) {
-    Serial.print("\n");
-    Serial.println("More Info:");
-    Serial.print("RSSI: ");
-    Serial.println(LoRa.packetRssi());
-    Serial.print("SNR: ");
-    Serial.println(LoRa.packetSnr());
-    Serial.print("Message ID: ");
-    printAddress(packet.msg_id, "");
-    Serial.print("\n");
-  }
-
-  Serial.println("-----[END MESSAGE]-----");
-
 }
 
 void splitCharArray(char* input, char delimiter, char** outputArray, int maxElements) {
@@ -226,6 +253,15 @@ void splitCharArray(char* input, char delimiter, char** outputArray, int maxElem
   while (token != NULL && elementCount < maxElements) {
     outputArray[elementCount++] = token;
     token = strtok(NULL, &delimiter);
+  }
+}
+
+void printArray(uint8_t* arr, char* del, int size) {
+  for (int i = 0; i < size; i++) {
+    if (i != 0) {
+      Serial.print(del);
+    }
+    Serial.print(arr[i], DEC);
   }
 }
 
@@ -242,6 +278,33 @@ bool compAddress(uint8_t* a, uint8_t* b) {
     }
   }
   return true;
+}
+
+bool compBuffer(uint8_t* a, uint8_t* b, uint8_t size) {
+  for (size_t i = 0; i < size; i++) {
+    if (a[i] != b[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void copyBuffer(uint8_t* a, uint8_t* b, uint8_t size) {
+  for (size_t i = 0; i < size; i++) {
+    a[i] == b[i];
+  }
+}
+
+void repeatBuffer(uint8_t* a, uint8_t b, uint8_t size) {
+  for (size_t i = 0; i < size; i++) {
+    a[i] == b;
+  }
+}
+
+void randomBuffer(uint8_t* a, uint8_t max, uint8_t size) {
+  for (size_t i = 0; i < size; i++) {
+    a[i] == random(max);
+  }
 }
 
 void printAddress(uint8_t* addr, char* delimiter) {
@@ -271,26 +334,4 @@ bool checkBucket(uint8_t* id) {
     }
   }
   return false;
-}
-
-/* Logging */
-void info(char* msg) {
-  if (LOG_INFO) {
-    Serial.print("[INFO] ");
-    Serial.print(msg);
-  }
-}
-
-void warning(char* msg) {
-  if (LOG_WARNING) {
-    Serial.print("[WARNING] ");
-    Serial.print(msg);
-  }
-}
-
-void error(char* msg) {
-  if (LOG_ERROR) {
-    Serial.print("[ERROR] ");
-    Serial.print(msg);
-  }
 }
