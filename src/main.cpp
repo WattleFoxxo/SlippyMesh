71f87ca47583b7553bc34a4ebb689972f99873a5
WattleFoxxo <88825700+WattleFoxxo@users.noreply.github.com>
2023-11-06 10:15:59 +1100

    Major update
    + 32 bit addressing

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,115 @@
+// #include "main.h"
+
+// uint8_t incomingBuffer[RH_MESH_MAX_MESSAGE_LEN];
+// uint8_t outgoingBuffer[RH_MESH_MAX_MESSAGE_LEN];
+
+// RH_RF95 radio;
+// RHMesh *manager;
+
+// void setup() {
+//     randomSeed(analogRead(0));
+// 	Serial.begin(115200);
+//     delay(500);
+
+//     uint32_t uuid = ADDR;
+//     // for (int i = 0; i < 4; i++) {
+//     //     uuid |= (uint32_t)UniqueID8[i+4] << (i * 8);
+//     // }
+
+//     Serial.println(F("Starting Slippy Mesh..."));
+
+//     Serial.print(F("Radio: "));
+//     manager = new RHMesh(radio, uuid);
+//     if (!manager->init()) {
+//         Serial.println(F("FAIL"));
+//         while(true);
+//     }
+
+//     radio.setTxPower(23, false);
+//     radio.setFrequency(916.0);
+//     radio.setCADTimeout(500);
+
+//     Serial.println(F("READY"));
+
+//     Serial.print(F("Mesh: "));
+
+//     // myAddress = random(5);
+//     // manager->setThisAddress(myAddress);
+
+//     Serial.println(F("READY"));
+
+//     Serial.print(F("Your address is: "));
+//     printAddress(manager->thisAddress());
+//     Serial.println();
+//     Serial.println(manager->thisAddress(), DEC);
+// }
+
+// void loop() {
+//     uint8_t len = sizeof(incomingBuffer);
+//     uint32_t from;
+//     if (manager->recvfromAckTimeout(incomingBuffer, &len, 500, &from)) {
+//         Serial.print("[");
+//         printAddress(from);
+//         Serial.print("]: ");
+//         Serial.println((char*)incomingBuffer);
+//     }
+
+//     if (Serial.available() > 0) {
+
+//         uint8_t addressStr[10];
+//         uint8_t addressStrLen = Serial.readBytesUntil(' ', addressStr, 10);
+//         addressStr[addressStrLen] = '\0';
+        
+//         uint8_t message[128];
+//         uint8_t messageLen = Serial.readBytesUntil('\n', message, 128);
+//         message[messageLen] = '\0';
+        
+//         uint32_t address = strtoul(addressStr, NULL, 16);
+
+//         Serial.print("Sending ... ");
+
+//         uint8_t error = manager->sendtoWait(message, messageLen, address);
+//         Serial.println(error);
+//     }
+// }
+
+// uint8_t sendPacket(uint32_t address, uint8_t* packet, uint8_t length) {
+//     uint8_t buffer[length];
+//     return manager->sendtoWait(buffer, length, address);
+// }
+
+// void printAddress(uint32_t address) {
+//     Serial.print("0x");
+//     for (int i = 7; i >= 0; i--) {
+//         uint8_t nibble = (address >> (i * 4)) & 0x0F;
+//         Serial.print(nibble, HEX);
+//     }
+// }
+
 #include "main.h"
 
-uint8_t localAddress = 0;
-uint8_t routes[SLIPPY_MAX_NODES];
-int16_t rssi[SLIPPY_MAX_NODES];
+uint8_t incomingBuffer[RH_MESH_MAX_MESSAGE_LEN];
+uint8_t outgoingBuffer[RH_MESH_MAX_MESSAGE_LEN];
 
 RH_RF95 radio;
 RHMesh *manager;
 
-Settings settings;
-
-uint8_t incomingBuffer[RH_MESH_MAX_MESSAGE_LEN];
-
-int freeMem() {
-  extern int __heap_start, *__brkval;
-  int v;
-  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
-}
-
 void setup() {
     randomSeed(analogRead(0));
-    Serial.begin(115200);
+	Serial.begin(115200);
     delay(500);
+
+    uint32_t uuid = 0;
+    for (int i = 0; i < 4; i++) {
+        uuid |= (uint32_t)UniqueID8[i+4] << (i * 8);
+    }
 
     Serial.println(F("Starting Slippy Mesh..."));
 
-    uint8_t EEPROM_INTEGRITY_CHECK[6];
-    EEPROM.get(EEPROM_ADDR_INTEGRITY, EEPROM_INTEGRITY_CHECK);
-
-    // Check if EEPROM is corrupted or not set
-    if (!strcmp((char*)EEPROM_INTEGRITY_CHECK, (char*)EEPROM_INTEGRITY)) {
-        EEPROM.get(EEPROM_ADDR_SETTINGS, settings);
-    } else {
-        Serial.println(F("Warning: the EEPROM was corrupted or not set, writing defaults."));
-
-        settings.prefered_address = SETTING_PREFERED_ADDRESS_DEFUALT;
-        settings.auto_restart = SETTING_AUTO_RESTART_DEFUALT;
-
-        EEPROM.put(EEPROM_ADDR_INTEGRITY, EEPROM_INTEGRITY);
-        EEPROM.put(EEPROM_ADDR_SETTINGS, settings);
-    }
-
-    manager = new RHMesh(radio, 0);
-
+    Serial.print(F("Radio: "));
+    manager = new RHMesh(radio, uuid);
     if (!manager->init()) {
-        Serial.println(F("The radio failed to start."));
+        Serial.println(F("FAIL"));
         while(true);
     }
 
@@ -51,375 +117,75 @@ void setup() {
     radio.setFrequency(916.0);
     radio.setCADTimeout(500);
 
-    for(uint8_t n=1;n<=SLIPPY_MAX_NODES;n++) {
-        routes[n-1] = 0;
-        rssi[n-1] = 0;
-    }
+    Serial.println(F("READY"));
 
-    localAddress = findAvalabeAddress(settings.prefered_address);
-    manager->setThisAddress(localAddress);
+    Serial.print(F("Mesh: "));
 
-    Serial.println(F("Slippy Mesh is ready!"));
+    Serial.println(F("READY"));
+
     Serial.print(F("Your address is: "));
-    Serial.println(localAddress);
-    Serial.print(F("Free memory: "));
-    Serial.println(freeMem());
-}
-
-const __FlashStringHelper* getErrorString(uint8_t error) {
-    switch(error) {
-        case 1:
-            return F("invalid length");
-            break;
-        case 2:
-            return F("no route");
-            break;
-        case 3:
-            return F("timeout");
-            break;
-        case 4:
-            return F("no reply");
-            break;
-        case 5:
-            return F("unable to deliver");
-            break;
-    }
-
-    return F("unknown");
-}
-
-void updateRoutingTable() {
-    for(uint8_t n = 1; n <= SLIPPY_MAX_NODES; n++) {
-        RHRouter::RoutingTableEntry *route = manager->getRouteTo(n);
-        if (n == localAddress) {
-            routes[n-1] = 255;
-        } else {
-            routes[n-1] = route->next_hop;
-            if (routes[n-1] == 0) {
-                rssi[n-1] = 0;
-            }
-        }
-    }
+    printAddress(manager->thisAddress());
+    Serial.println();
+    Serial.println(manager->thisAddress(), DEC);
 }
 
 void loop() {
     uint8_t len = sizeof(incomingBuffer);
-    uint8_t from;
-    if (manager->recvfromAckTimeout((uint8_t *)incomingBuffer, &len, 2000, &from)) {
-        incomingBuffer[len] = '\0';
-
-        Packet packet;
-        deserializePacket(incomingBuffer, packet);
-
-        if (packet.network_version != SLIPPY_NETWORK_VERSION) {
-            return;
-        }
-
-        unsigned char base64[(((4 * packet.length / 3) + 3) & ~3)]; // Calculate size of base64 string. 
-        encode_base64(packet.data, packet.length, base64);
-
-        if (packet.packet_type == SLIPPY_PACKET_DIRECT) {
-            Serial.println(F("-- Message --"));
-            Serial.print(F("Address: "));
-            Serial.println(from, DEC);
-            Serial.print(F("Data: "));
-            printBytes(packet.data, packet.length);
-            Serial.print(F("\nJSON: "));
-            Serial.print(F("\"message\":{\"address\":"));
-            Serial.print(from);
-            Serial.print(F(",\"data\":\""));
-            Serial.print((char*)base64);
-            Serial.println(F("\"}"));
-            Serial.println(F("-------------"));
-
-            remoteEXE(from, packet);
-        } else if (packet.packet_type == SLIPPY_PACKET_BROADCAST) {
-            Serial.println(F("-- Broadcast Message --"));
-            Serial.print(F("Address: "));
-            Serial.println(from, DEC);
-            Serial.print(F("Data: "));
-            printBytes(packet.data, packet.length);
-            Serial.print(F("\nJSON: "));
-            Serial.print(F("\"broadcast_message\":{\"address\":"));
-            Serial.print(from);
-            Serial.print(F(",\"data\":"));
-            Serial.print((char*)base64);
-            Serial.println(F("}"));
-            Serial.println(F("-----------------------"));
-        }
+    uint32_t from;
+    if (manager->recvfromAckTimeout(incomingBuffer, &len, 500, &from)) {
+        incomingBuffer[len] = '/0';
+        Serial.print("[");
+        printAddress(from);
+        Serial.print("]: ");
+        Serial.println((char*)incomingBuffer);
     }
 
     if (Serial.available() > 0) {
-        String cmd = Serial.readStringUntil(' ');
-        cmd.trim();
 
-        if (cmd == "help") cmdHelp();
-        else if (cmd == "info") cmdInfo();
-        else if (cmd == "send") cmdSend();
-        else if (cmd == "send64") cmdSend64();
-        else if (cmd == "set_address") cmdSetAddress();
-        else if (cmd == "set_auto_restart") cmdSetAutoRestart();
-        else if (cmd == "nuke_all_the_settings_please") cmdWipe();
-        else if (cmd == "restart") cmdRestart();
-        else cmdUnrecognized(cmd);
-    }
-
-    if (settings.auto_restart) {
-        if ((millis() / 60000) > settings.auto_restart) {
-            resetFunc();
-        }
-    }
-}
-
-bool sendMessage(uint8_t address, uint8_t* message, uint8_t len) {
-    uint8_t packetLen = SLIPPY_HEADER_SIZE + len;
-    uint8_t packetBuff[packetLen];
-
-    Packet packet;
-    packet.network_version = SLIPPY_NETWORK_VERSION;
-    packet.length = len;
-    strcpy(packet.data, message);
-
-    updateRoutingTable();
-
-    if (address == 255) {
-        packet.packet_type = SLIPPY_PACKET_DIRECT;
+        uint8_t addressStr[10];
+        uint8_t addressStrLen = Serial.readBytesUntil(' ', addressStr, 10);
+        addressStr[addressStrLen] = '\0';
         
-        serializePacket(packet, packetBuff);
-        Serial.println(F("Sending message to every node, this will take a while..."));
-
-        for (uint8_t addr = 1; addr < SLIPPY_MAX_NODES; addr++) {
-            Serial.print(F("Sending message to "));
-            Serial.print(addr);
-            Serial.print(F("... "));
-
-            uint8_t error = manager->sendtoWait(packetBuff, packetLen, addr);
-            if (error != RH_ROUTER_ERROR_NONE) {
-                Serial.print(F("ERROR: "));
-                Serial.println(getErrorString(error));
-            } else {
-                Serial.println(F("OK"));
-            }
-        }
-
-        Serial.println(F("Done!"));
-        return true;
-    } else {
-        packet.packet_type = SLIPPY_PACKET_DIRECT;
+        uint8_t message[SLIPPY_PACKET_DATA_SIZE];
+        uint8_t messageLen = Serial.readBytesUntil('\n', message, SLIPPY_PACKET_DATA_SIZE);
+        message[messageLen] = '\0';
         
-        serializePacket(packet, packetBuff);
+        uint32_t address = strtoul(addressStr, NULL, 16);
 
-        Serial.print(F("Sending message to "));
-        Serial.print(address);
-        Serial.print(F("... "));
+        Serial.print(messageLen, DEC);
 
-        uint8_t error = manager->sendtoWait(packetBuff, packetLen, address);
-        if (error != RH_ROUTER_ERROR_NONE) {
-            Serial.print(F("ERROR: "));
-            Serial.println(getErrorString(error));
-        } else {
-            Serial.println(F("OK"));
-            return true;
-        }
-    }
-    return false;
-}
-
-void serializePacket(const Packet& packet, uint8_t* byteArray) {
-    byteArray[0] = packet.network_version;
-    byteArray[1] = packet.packet_type;
-    byteArray[2] = packet.length;
-
-    for (uint8_t i = 0; i < packet.length; i++) {
-        byteArray[i + 3] = packet.data[i];
+        uint8_t error = sendMessaage(address, message, messageLen);
+        Serial.println(error);
     }
 }
 
-void deserializePacket(const uint8_t* byteArray, Packet& packet) {
-    packet.network_version = byteArray[0];
-    packet.packet_type = byteArray[1];
-    packet.length = byteArray[2];
+uint8_t sendMessaage(uint32_t address, uint8_t* data, uint8_t length, uint8_t service=SLIPPY_PACKET_SERVICE_TXT) {
 
-    for (uint8_t i = 0; i < packet.length; i++) {
-        packet.data[i] = byteArray[i + 3];
-    }
+    SlippyPacketNormal newPacket;
+
+    newPacket.service = service;
+    newPacket.uid = random(0xffffffff);
+    newPacket.size = length;
+    memcpy(newPacket.data, data, length);
+
+    uint8_t size = (sizeof(newPacket)-200)+length;
+
+    Serial.println(size);
+
+    uint8_t buffer[size];
+    memcpy(buffer, &newPacket, size);
+
+    Serial.println((char*)buffer);
+    // Serial.println(sizeof(newPacket), DEC);
+    // Serial.println(length, DEC);
+
+    return manager->sendtoWait(buffer, sizeof(newPacket), address);
 }
 
-void printBytes(uint8_t* byteArray, uint8_t len) {
-    for (uint8_t i = 0; i < len; i++) {
-        Serial.write(byteArray[i]);
-    }
-}
-
-uint8_t findAvalabeAddress(uint8_t addr) {
-    Serial.println(F("Looking for avalable address..."));
-
-    Packet packet;
-    packet.network_version = SLIPPY_NETWORK_VERSION;
-    packet.packet_type = SLIPPY_PACKET_PING;
-    packet.length = 4;
-    strcpy(packet.data, "PING");
-
-    uint8_t packetBuff[7];
-
-    serializePacket(packet, packetBuff);
-
-    if (!settings.prefered_address) {
-        addr = random(253) + 1;
-    }
-
-    while (true) {
-        updateRoutingTable();
-
-        uint8_t error = manager->sendtoWait(packetBuff, 7, addr);
-        if (error == RH_ROUTER_ERROR_NO_ROUTE) {
-            Serial.print(addr);
-            Serial.println(F(" is available!"));
-            if (!settings.prefered_address) {
-                settings.prefered_address = addr;
-                EEPROM.put(EEPROM_ADDR_SETTINGS, settings);
-            }
-
-            return addr;
-        } else {
-            Serial.print(addr);
-            Serial.println(F(" is taken."));
-        }
-       
-        addr = random(253) + 1;
-    }
-}
-
-/* Serial Commands */
-
-void cmdUnrecognized(String cmd) {
-	Serial.print("Error: Unrecognized command \"");
-	Serial.print(cmd);
-	Serial.println("\". Type \"help\" for the list of commands.");
-}
-
-void cmdHelp() {
-	Serial.println(F("-- Help --"));
-    Serial.println(F("Messaging"));
-    Serial.println(F("    Send message: \"send <address: byte> <message: string>\". Examples: \"send 1 hello\", \"send 255 hello everyone\" (this is very slow)"));
-    Serial.println(F("    Send base64 string: \"send64 <address: byte> <message: string>\". Examples: \"send64 1 YWxleGQ=\", \"send64 255 aGVsbG8gZXZlcnlvbmU=\" (this is very slow)"));
-    Serial.println(F("Settings"));
-    Serial.println(F("    Set your preferred address: \"set_address <address: byte>\". Examples: \"set_address 4\", \"set_address 0\" (this will pick a random address)"));
-    Serial.println(F("    Set the auto restart timer: \"set_auto_restart <time: byte>\". Examples: \"set_auto_restart 30\" (the devce restart every 30 minutes), \"set_auto_restart 0\" (this will disable it)"));
-    Serial.println(F("    Reset all the settings: \"nuke_all_the_settings_please\""));
-    Serial.println(F("Other"));
-    Serial.println(F("    Get basic information: \"info\""));
-    Serial.println(F("    Restart the device: \"restart\""));
-    Serial.println(F("    Get help: \"help\""));
-    Serial.println(F("For more info go to https://github.com/WattleFoxxo/SlippyMesh/"));
-    Serial.println(F("----------"));
-}
-
-void cmdInfo() {
-    Serial.println(F("-- Info --"));
-    Serial.print(F("Address: "));
-    Serial.println(localAddress, HEX);
-    Serial.print(F("Device Version: "));
-    Serial.println(radio.getDeviceVersion());
-    Serial.print(F("Uptime: "));
-    Serial.println(millis());
-    Serial.print(F("JSON: "));
-    Serial.print(F("\"info\":{\"address\":"));
-    Serial.print(localAddress);
-    Serial.print(F(",\"network_version\":"));
-    Serial.print(SLIPPY_NETWORK_VERSION);
-    Serial.print(F(",\"uptime\":"));
-    Serial.print(millis());
-    Serial.println(F("}"));
-    Serial.println(F("----------"));
-}
-
-void cmdSend() {
-    uint8_t address = Serial.readStringUntil(' ').toInt();
-    String message = Serial.readStringUntil('\n');
-    
-    if (message.length() > SLIPPY_MESSAGE_SIZE) {
-        Serial.print(F("Error: Maximum message length is "));
-        Serial.print(SLIPPY_MESSAGE_SIZE);
-        Serial.println(F(" characters."));
-        return;
-    }
-
-    sendMessage(address, message.c_str(), message.length());
-}
-
-void cmdSend64() {
-    uint8_t address = Serial.readStringUntil(' ').toInt();
-    String base64 = Serial.readStringUntil('\n');
-    uint8_t message[63];
-
-    uint8_t len = decode_base64(base64.c_str(), message);
-    message[len] = '\0';
-
-    sendMessage(address, message, strlen(message));
-}
-
-void cmdWipe() {
-    Serial.println(F("Warning: This will WIPE all saved settings! are you sure you want to do this? (y/N)"));
-    while (Serial.available() == 0);
-
-    String answer = Serial.readStringUntil('\n');
-    answer.trim();
-    answer.toLowerCase();
-
-    if (answer == "y") {
-        EEPROM.write(EEPROM_ADDR_INTEGRITY, 0); // Corrupt the eeprom integrity.
-        Serial.println(F("Done, restarting..."));
-        
-        delay(500);
-        resetFunc(); // Restart the device when done.
-
-        return; // If this gets called something is very very wrong.
-    }
-
-    Serial.println(F("Canceled."));
-}
-
-void cmdSetAddress() {
-    uint8_t address = Serial.readStringUntil('\n').toInt();
-    settings.prefered_address = address;
-
-    EEPROM.put(EEPROM_ADDR_SETTINGS, settings);
-    Serial.println(F("Set your address, restarting..."));
-    
-    delay(500);
-    resetFunc(); // Restart the device when done.
-}
-
-void cmdSetAutoRestart() {
-    uint8_t time = Serial.readStringUntil('\n').toInt();
-    settings.auto_restart = time;
-
-    EEPROM.put(EEPROM_ADDR_SETTINGS, settings);
-    Serial.println(F("Set auto restart time, restarting..."));
-    
-    delay(500);
-    resetFunc(); // Restart the device when done.
-}
-
-void cmdRestart() {
-    Serial.println(F("Restarting..."));
-    delay(500);
-    resetFunc(); // Restart the device
-}
-
-/* Remote EXE Commands */
-
-void remoteEXE(uint8_t from, Packet packet) {
-    packet.data[packet.length] = '\0';
-    
-    if (!strcmp(packet.data, "/ping")) {
-        String message = "pong";
-        sendMessage(from, message.c_str(), message.length());
-    }
-
-    if (!strcmp(packet.data, "/uptime")) {
-        String message = "uptime: "+String(millis())+" ms";
-        sendMessage(from, message.c_str(), message.length());
+void printAddress(uint32_t address) {
+    Serial.print("0x");
+    for (int i = 7; i >= 0; i--) {
+        uint8_t nibble = (address >> (i * 4)) & 0x0F;
+        Serial.print(nibble, HEX);
     }
 }
--- src/main.h
+++ src/main.h
@@ -3,61 +3,48 @@
 
 #include <Arduino.h>
 #include <EEPROM.h>
+#include <SPI.h>
 
-#include <RHRouter.h>
+#include <ArduinoUniqueID.h>
+
 #include <RHMesh.h>
 #include <RH_RF95.h>
 
-#include <base64.hpp>
+#define SLIPPY_LOCAL_BROADCAST_ADDRESS 0x000000ff
+#define SLIPPY_GLOBAL_BROADCAST_ADDRESS 0xffffffff
 
+#define SLIPPY_PACKET_TYPE_NORMAL 0
+#define SLIPPY_PACKET_TYPE_CHAIN 1
 
-#define SETTING_PREFERED_ADDRESS_DEFUALT 0 // Set this to your prefed address. Set to zero to disable. Addresses range from 1 to 254.
-#define SETTING_AUTO_RESTART_DEFUALT 0 // Automaticly restart your device. Set to zero to disable. Value is in minutes.
+#define SLIPPY_PACKET_SERVICE_TXT 0
 
-#define SLIPPY_NETWORK_VERSION 1
-#define SLIPPY_PACKET_PING 0
-#define SLIPPY_PACKET_DIRECT 1
-#define SLIPPY_PACKET_BROADCAST 2
-#define SLIPPY_MESSAGE_SIZE 200
-#define SLIPPY_MAX_NODES 254
+#define SLIPPY_PACKET_DATA_SIZE 200
 
-#define EEPROM_ADDR_INTEGRITY 0
-#define EEPROM_ADDR_SETTINGS 6
+struct SlippyPacketNormal {
+    uint8_t type = SLIPPY_PACKET_TYPE_NORMAL;
+    uint8_t service = SLIPPY_PACKET_SERVICE_TXT;
+    uint8_t flags = 0x00;
+    uint32_t uid;
 
-const uint8_t EEPROM_INTEGRITY[6] = {0x41, 0x6C, 0x65, 0x78, 0x44, 0x00};
-
-struct Settings {
-    uint8_t prefered_address;
-    bool auto_restart;
+    uint8_t size;
+    uint8_t data[SLIPPY_PACKET_DATA_SIZE];
 };
 
-struct Packet {
-    uint8_t network_version;
-    uint8_t packet_type;
-    uint8_t length;
-    uint8_t data[SLIPPY_MESSAGE_SIZE];
+struct SlippyPacketChain {
+    uint8_t type = SLIPPY_PACKET_TYPE_CHAIN;
+    uint8_t service = SLIPPY_PACKET_SERVICE_TXT;
+    uint8_t flags = 0x00;
+    uint32_t uid;
+
+    uint8_t index;
+    uint8_t count;
+    uint8_t size;
+    uint8_t data[SLIPPY_PACKET_DATA_SIZE];
 };
-const uint8_t SLIPPY_HEADER_SIZE = sizeof(Packet) - SLIPPY_MESSAGE_SIZE;
-
-
-bool sendMessage(uint8_t address, uint8_t* message);
-void serializePacket(const Packet& packet, uint8_t* byteArray);
-void deserializePacket(const uint8_t* byteArray, Packet& packet);
-void printBytes(uint8_t* byteArray, uint8_t len);
-uint8_t findAvalabeAddress(uint8_t addr);
-
-void cmdUnrecognized(String cmd);
-void cmdHelp();
-void cmdInfo();
-void cmdSend();
-void cmdSend64();
-void cmdWipe();
-void cmdSetAddress();
-void cmdSetAutoRestart();
-void cmdRestart();
-
-void remoteEXE(uint8_t from, Packet packet);
 
 void(* resetFunc)(void) = 0;
+
+uint8_t sendMessaage(uint32_t address, uint8_t* data, uint8_t length, uint8_t service=SLIPPY_PACKET_SERVICE_TXT);
+void printAddress(uint32_t address);
 
 #endif
